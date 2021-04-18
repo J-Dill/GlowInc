c876c31b810da5b2c8b7c5970d705d2b765eed1d
Dylan <dylan.jech@mnsu.edu>
2021-04-18 01:21:11 -0500

    Spawn Glow Ball on throw

--- src/main/java/com/github/jdill/glowinc/blocks/GlowBallBlock.java
+++ src/main/java/com/github/jdill/glowinc/blocks/GlowBallBlock.java
@@ -1,17 +1,29 @@
 package com.github.jdill.glowinc.blocks;
 
+import javax.annotation.Nullable;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.DirectionProperty;
+import net.minecraft.state.StateContainer;
+import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.util.Direction;
+import net.minecraft.util.Mirror;
+import net.minecraft.util.Rotation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.IWorld;
+import net.minecraft.world.IWorldReader;
 
 public class GlowBallBlock extends Block {
 
     protected static final VoxelShape SHAPE = Block.makeCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 1.0D, 16.0D);
+    private static final DirectionProperty FACING = BlockStateProperties.FACING;
 
     public static final String ID = "glow_ball";
 
@@ -27,6 +39,61 @@ public class GlowBallBlock extends Block {
 
     public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
         return SHAPE;
+    }
+
+    /**
+     * Only allows the Glow Ball to be placed on the solid side of blocks, and only solid blocks.
+     */
+    @Override
+    public boolean isValidPosition(BlockState state, IWorldReader worldIn, BlockPos pos) {
+        Direction direction = state.get(FACING);
+        BlockPos blockpos = pos.offset(direction.getOpposite());
+        BlockState blockstate = worldIn.getBlockState(blockpos);
+        return blockstate.isSolidSide(worldIn, blockpos, direction);
+    }
+
+    /**
+     * Searches for a valid wall to place on when placed on a block.
+     */
+    @Nullable
+    public BlockState getStateForPlacement(BlockItemUseContext context) {
+        BlockState blockstate = this.getDefaultState();
+        IWorldReader iworldreader = context.getWorld();
+        BlockPos blockpos = context.getPos();
+        Direction[] adirection = context.getNearestLookingDirections();
+
+        for(Direction direction : adirection) {
+            Direction direction1 = direction.getOpposite();
+            blockstate = blockstate.with(FACING, direction1);
+            if (blockstate.isValidPosition(iworldreader, blockpos)) {
+                return blockstate;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * When the block the Glow Ball is placed on is broken, also break the Tree Tap.
+     */
+    @Override
+    public BlockState updatePostPlacement(BlockState stateIn, Direction facing, BlockState facingState, IWorld worldIn, BlockPos currentPos, BlockPos facingPos) {
+        return facing.getOpposite() == stateIn.get(FACING) && !stateIn.isValidPosition(worldIn, currentPos) ? Blocks.AIR.getDefaultState() : stateIn;
+    }
+
+    @Override
+    public BlockState rotate(BlockState state, Rotation rot) {
+        return state.with(FACING, rot.rotate(state.get(FACING)));
+    }
+
+    @Override
+    public BlockState mirror(BlockState state, Mirror mirrorIn) {
+        return state.rotate(mirrorIn.toRotation(state.get(FACING)));
+    }
+
+    @Override
+    protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
     }
 
 }
--- src/main/java/com/github/jdill/glowinc/entity/projectile/GlowBallEntity.java
+++ src/main/java/com/github/jdill/glowinc/entity/projectile/GlowBallEntity.java
@@ -1,6 +1,7 @@
 package com.github.jdill.glowinc.entity.projectile;
 
 import com.github.jdill.glowinc.Registry;
+import net.minecraft.block.BlockState;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.IRendersAsItem;
 import net.minecraft.entity.LivingEntity;
@@ -10,7 +11,12 @@ import net.minecraft.network.IPacket;
 import net.minecraft.particles.IParticleData;
 import net.minecraft.particles.ItemParticleData;
 import net.minecraft.particles.ParticleTypes;
+import net.minecraft.util.Direction;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.math.RayTraceResult.Type;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
@@ -41,6 +47,19 @@ public class GlowBallEntity extends ProjectileItemEntity implements IRendersAsIt
     @Override
     protected void func_230299_a_(BlockRayTraceResult result) {
         super.func_230299_a_(result);
+        if (result.getType().equals(Type.BLOCK)) {
+            BlockPos pos = result.getPos();
+            Direction direction = result.getFace();
+            BlockPos blockPos = pos.offset(direction);
+            BlockState blockState = this.world.getBlockState(blockPos);
+            if (blockState.isAir()) {
+                this.world.setBlockState(blockPos, Registry.GLOW_BALL_BLOCK.get().getDefaultState());
+                this.world.playSound(blockPos.getX(), blockPos.getY(), blockPos.getZ(),
+                    SoundEvents.BLOCK_SLIME_BLOCK_PLACE, SoundCategory.BLOCKS,
+                    1.0f, 1.0f, true
+                );
+            }
+        }
     }
 
     @Override
